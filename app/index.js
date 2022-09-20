b030c423d91c2f143b7f1367ffbe9513e125f13d
John Watson <johnwatson484@hotmail.com>
2022-09-20 12:04:45 +0100

    Add dependencies to package.json

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 const server = require('./server')
+const db = require('./data')
 
 const init = async () => {
   await server.start()
   console.log('Server running on %s', server.info.uri)
+  const examples = await db.example.findAll()
+  console.table(examples)
 }
 
 process.on('unhandledRejection', (err) => {
