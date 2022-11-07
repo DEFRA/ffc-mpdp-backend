1ddfaf09f5e402e8adb305d67975e926c5221313
vmuthu-kn <velmuruganm@kainos.com>
2022-11-07 14:51:51 +0000

    upated the index file

--- app/index.js
+++ app/index.js
@@ -2,12 +2,7 @@ const createServer = require('./server')
 const databaseConfig = require('./config/database-config')
 const { environments } = require('./config/constants')
 
-createServer()
-  .then(server => server.start())
-  .catch(err => {
-    console.log(err)
-    process.exit(1)
-  })
+
 
 const value = {
   database: databaseConfig,
@@ -16,4 +11,11 @@ const value = {
   env: process.env.NODE_ENV || environments.development
 }
 
+createServer()
+  .then(server => server.start())
+  .catch(err => {
+    console.log(err)
+    process.exit(1)
+  })
+  
 module.exports = value
