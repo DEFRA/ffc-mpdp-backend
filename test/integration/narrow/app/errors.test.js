b1b87e824e0eb439536a8168522a4a364ea144ce
vmuthu-kn <velmuruganm@kainos.com>
2022-11-15 15:37:55 +0000

    updated the test case

--- test/integration/narrow/app/errors.test.js
+++ test/integration/narrow/app/errors.test.js
@@ -1,28 +1,31 @@
 describe('errors plugin test', () => {
+  const createServer = require('../../../../app/server')
+  let server
+
+  beforeEach(async () => {
+    server = await createServer()
+    await server.start()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
   test('createServer returns server', () => {
     const errors = require('../../../../app/plugins/errors')
     expect(errors).toBeDefined()
   })
 
-  test('test the invalid URL for 500 status', async () => {
-    const createServer = require('../../../../app/server')
-    const server = await createServer()
-    await server.start()
+  test('test the invalid URL for 404 status', async () => {
     const options = {
       method: 'GET',
-      url: '/paymentdata'
+      url: '/invalidurl'
     }
     const response = await server.inject(options)
     expect(response.statusCode).toBe(500)
   }, 30000)
 
   test('returns the error when the server called with error URL', async () => {
-    const createServer = require('../../../../app/server')
-    const server = await createServer()
-    await server.start()
-    console.log('server.registrations : ')
-    console.log(server.registrations)
-
     expect(server.registrations['hapi-pino']).toBeDefined()
     expect(server.registrations.errors).toBeDefined()
   }, 30000)
