658e4ba7e8cf9dce292eca67801d68d25fc84698
vmuthu <112011276+vmuthu-kn@users.noreply.github.com>
2022-11-15 17:52:30 +0000

    logger plugin setup (#14)
    * errors plugin added
    
    * version updated
    
    * updated test case to reflect timeout
    
    * formatted values
    
    * updated the test case
    
    * updated the test case
    
    * updated the test case name

---
+++ test/integration/narrow/app/errors.test.js
@@ -0,0 +1,32 @@
+describe('errors plugin test', () => {
+  const createServer = require('../../../../app/server')
+  let server
+
+  beforeEach(async () => {
+    server = await createServer()
+    await server.start()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  test('errors plugin should be defined', () => {
+    const errors = require('../../../../app/plugins/errors')
+    expect(errors).toBeDefined()
+  })
+
+  test('test the invalid URL for 404 status', async () => {
+    const options = {
+      method: 'GET',
+      url: '/invalidurl'
+    }
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(404)
+  }, 30000)
+
+  test('Verify the plugins are registered to the server', async () => {
+    expect(server.registrations['hapi-pino']).toBeDefined()
+    expect(server.registrations.errors).toBeDefined()
+  }, 30000)
+})
