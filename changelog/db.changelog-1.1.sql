1e91938f55ecfd3052e6436253fc203c747af36c
vmuthu-kn <velmuruganm@kainos.com>
2022-11-16 17:12:03 +0000

    converted the change set to xml as required in ADO

--- changelog/db.changelog-1.1.sql
+++
@@ -1,5 +0,0 @@
---changeset author:VMuthu  
-ALTER TABLE payment_activity_data
-  ADD COLUMN scheme_detail character varying(128),
-  ADD COLUMN activity_level character varying(16);
-ALTER TABLE payment_activity_data DROP COLUMN activity_detail;
