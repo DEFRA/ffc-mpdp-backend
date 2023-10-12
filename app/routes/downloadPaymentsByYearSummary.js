bd7a0e36c852fc98a68ec85355b947cbfbcc19ee
Afshin <147500504+Afshin-Karamifar-Capgemini@users.noreply.github.com>
2023-10-12 11:42:45 +0100

    MPDP-542-Download Payments Data by Year end point  (#70)
    * MPDP-542 Download Payments Data by Year End-Point has been added
    
    * MPDP-542 Package Version updated
    
    * MPDP-542-Eslint errors fixed
    
    * Simplified by Sid
    
    * Disable Eslint for camelcase
    
    * Column Title updated

---
+++ app/routes/downloadPaymentsByYearSummary.js
@@ -0,0 +1,33 @@
+const { Parser } = require('json2csv')
+const { getSchemePaymentsByYear } = require('../services/databaseService')
+
+module.exports = {
+  method: 'GET',
+  path: '/downloadPaymentsByYearSummary',
+  handler: async (_request, _response) => {
+    const fields = [
+      'financial year',
+      'scheme',
+      'amount'
+    ]
+    try {
+      const csvParser = new Parser({ fields })
+      const schemePaymentsByYear = (await getSchemePaymentsByYear()).sort((r1, r2) => r1.financial_year > r2.financial_year ? 1 : -1)
+      // eslint-disable-next-line camelcase
+      const schemePayments = schemePaymentsByYear.map(({ total_amount, financial_year, ...rest }) => ({
+        'financial year': financial_year,
+        amount: total_amount,
+        ...rest
+      }))
+      const csv = csvParser.parse(schemePayments)
+
+      return _response.response(csv)
+        .type('text/csv')
+        .header('Connection', 'keep-alive')
+        .header('Cache-Control', 'no-cache')
+        .header('Content-Disposition', 'attachment;filename=ffc-payments-by-year.csv')
+    } catch (error) {
+      return _response.response('Error while reading data' + error).code(500)
+    }
+  }
+}
---
+++ test/integration/narrow/routes/downloadPaymentsByYearSummary.test.js
@@ -0,0 +1,35 @@
+const csvPaymentsByYearSummaryTestData = require('./csvPaymentsByYearSummaryTestData.json')
+const { schemePaymentsModel } = require('../../../../app/services/databaseService')
+
+describe('downloadPaymentsByYearSummary test', () => {
+  const createServer = require('../../../../app/server')
+  let server
+
+  beforeEach(async () => {
+    server = await createServer()
+    await server.start()
+  })
+
+  // Test to check that the response is a csv file
+  test('GET /downloadPaymentsByYearSummary route returns csv file', async () => {
+    const options = {
+      method: 'GET',
+      url: '/downloadPaymentsByYearSummary'
+    }
+    const mockDb = jest.spyOn(schemePaymentsModel, 'findAll')
+    mockDb.mockResolvedValue(csvPaymentsByYearSummaryTestData)
+    const response = await server.inject(options)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.headers['content-type']).toContain('text/csv')
+    expect(response.headers['content-disposition']).toContain('attachment')
+    expect(response.result).toContain('"financial year","scheme","amount"')
+    expect(response.result).toContain('"21/22","Sustainable Farming Incentive Pilot","1436025.00"')
+    expect(response.result).toContain('"21/22","Farming Equipment and Technology Fund","1125893.00"')
+  })
+
+  afterEach(async () => {
+    jest.clearAllMocks()
+    await server.stop()
+  })
+})
