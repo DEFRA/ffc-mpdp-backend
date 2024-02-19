84de345d7f8488416c40b1619cae76856a898c40
Siddharth Godhania <siddharth.godhania@capgemini.com>
2024-02-19 11:56:36 +0000

    MPDP-573 Adding async csv file creation to BE

--- app/routes/downloadall.js
+++ app/routes/downloadall.js
@@ -1,5 +1,5 @@
-const { Parser } = require('json2csv')
 const { getRawData } = require('../services/databaseService')
+const { AsyncParser } = require('@json2csv/node')
 
 const cache = require('../cache')
 const config = require('../config/appConfig')
@@ -9,22 +9,22 @@ module.exports = {
   path: '/downloadall',
   handler: async (_request, res) => {
     try {
-      console.log('Getting CSV from cache')
       let csvData = await cache.get(config.cacheConfig.segments.csvData.name, 'csvData')
       if (!csvData || !Object.keys(csvData).length) {
-        console.log('Cached CSV not found, generating CSV.')
+        console.log('Cached CSV not found, generating CSV data.')
         const paymentData = await getRawData()
-        csvData = new Parser({ fields: config.csvFields }).parse(paymentData)
-        console.log('CSV generated')
+        csvData = JSON.stringify(paymentData)
+        console.log('Caching CSV data')
         await cache.set(config.cacheConfig.segments.csvData.name, 'csvData', csvData)
       }
 
-      console.log('Sending csv back')
-      return res.response(csvData)
-        .type('text/csv')
-        .header('Connection', 'keep-alive')
-        .header('Cache-Control', 'no-cache')
-        .header('Content-Disposition', 'attachment;filename=ffc-payment-data.csv')
+      const parser = new AsyncParser({ fields: config.csvFields })
+      const stream = parser.parse(csvData)
+      return res.response(stream)
+            .type('text/csv')
+            .header('Connection', 'keep-alive')
+            .header('Cache-Control', 'no-cache')
+            .header('Content-Disposition', 'attachment;filename=ffc-payment-data.csv')
     } catch (err) {
       console.error(err)
       throw err
