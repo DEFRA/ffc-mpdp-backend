f961c54f6a3e7a7fe2132dc2baab61387d03a741
John Watson <42574977+johnwatson484@users.noreply.github.com>
2024-11-19 15:47:50 +0000

    Add consistent CSV endpoint (#104)

--- app/data/payments.js
+++ app/data/payments.js
@@ -1,9 +1,25 @@
 const { Parser } = require('json2csv')
 const { getAllPayments } = require('./database')
+const { getPaymentData } = require('./search')
 
 /*
 Not current in use in front end due to workaround.  Needs updating to stream responses to client
 */
+
+async function getPaymentsCsv ({ searchString, limit, offset, sortBy, filterBy, action = 'download' }) {
+  const fields = [
+    'payee_name',
+    'part_postcode',
+    'town',
+    'county_council',
+    'amount'
+  ]
+  const { rows: payments } = await getPaymentData({ searchString, limit, offset, sortBy, filterBy, action })
+  const paymentsWithAmounts = payments.map(x => ({ ...x, amount: getReadableAmount(parseFloat(x.total_amount)) }))
+  const csvParser = new Parser({ fields })
+  return csvParser.parse(paymentsWithAmounts)
+}
+
 async function getAllPaymentsCsv () {
   const fields = [
     'financial_year',
@@ -16,11 +32,23 @@ async function getAllPaymentsCsv () {
     'scheme_detail',
     'amount'
   ]
-  const paymentData = await getAllPayments()
+  const payments = await getAllPayments()
   const csvParser = new Parser({ fields })
-  return csvParser.parse(paymentData)
+  return csvParser.parse(payments)
+}
+
+function getReadableAmount (amount) {
+  if (typeof amount !== 'number') {
+    return '0'
+  }
+
+  return amount.toLocaleString('en-GB', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  })
 }
 
 module.exports = {
+  getPaymentsCsv,
   getAllPaymentsCsv
 }
--- app/routes/payments.js
+++ app/routes/payments.js
@@ -1,6 +1,6 @@
 const Joi = require('joi')
 const { getPaymentData } = require('../data/search')
-const { getAllPaymentsCsv } = require('../data/payments')
+const { getAllPaymentsCsv, getPaymentsCsv } = require('../data/payments')
 
 module.exports = [{
   method: 'POST',
@@ -24,6 +24,29 @@ module.exports = [{
     },
     handler: async (request, h) => {
       const payments = await getPaymentData(request.payload)
+      return h.response(payments).code(200)
+    }
+  }
+},
+{
+  method: 'POST',
+  path: '/v1/payments/file',
+  options: {
+    validate: {
+      payload: {
+        searchString: Joi.string().trim().min(1).required(),
+        sortBy: Joi.string().default('score'),
+        filterBy: Joi.object({
+          schemes: Joi.array().items(Joi.string().lowercase()),
+          counties: Joi.array().items(Joi.string().lowercase()),
+          amounts: Joi.array().items(Joi.string()),
+          years: Joi.array().items(Joi.string())
+        }).default({})
+      },
+      failAction: async (_request, h, error) => h.response(error.toString()).code(400).takeover()
+    },
+    handler: async (request, h) => {
+      const payments = await getPaymentsCsv(request.payload)
       return h.response(payments).code(200)
     }
   }
