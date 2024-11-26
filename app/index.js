d4bc0bd938c814d0eb0a9f0573926475f560e454
John Watson <john.watson1@defra.gov.uk>
2024-11-26 12:49:16 +0000

    Add startup tests

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
-require('./insights').setup()
+const { setup: setupAppInsights } = require('./insights')
 const { createServer } = require('./server')
 
 async function startServer () {
+  setupAppInsights()
   const server = await createServer()
   await server.start()
   console.log(`Server running at: ${server.info.uri}`)
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,6 +1,9 @@
 jest.mock('../../app/insights')
+const { setup } = require('../../app/insights')
+
 jest.mock('../../app/server')
 const { createServer } = require('../../app/server')
+
 const { startServer } = require('../../app')
 
 const mockServer = {
@@ -14,6 +17,11 @@ describe('startServer', () => {
   beforeEach(() => {
     jest.clearAllMocks()
     createServer.mockResolvedValue(mockServer)
+  })
+
+  test('should setup application insights', async () => {
+    await startServer()
+    expect(setup).toHaveBeenCalledTimes(1)
   })
 
   test('should create a new server instance', async () => {
