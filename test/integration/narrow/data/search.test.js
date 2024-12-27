9a05287f4e6ff86c63ebf48a7630837209693b41
John Watson <john.watson1@defra.gov.uk>
2024-12-27 14:33:10 +0000

    Update tests

--- test/integration/narrow/data/search.test.js
+++ test/integration/narrow/data/search.test.js
@@ -4,8 +4,8 @@ const { getAllPayments } = require('../../../../app/data/database')
 const { getPaymentData, getSearchSuggestions } = require('../../../../app/data/search')
 
 getAllPayments.mockResolvedValue([
-  { payee_name: 'payee name', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 1', financial_year: '20/21', total_amount: 100 },
-  { payee_name: 'payee name', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 2', financial_year: '20/21', total_amount: 200 },
+  { payee_name: 'payee name 1', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 1', financial_year: '20/21', total_amount: 100 },
+  { payee_name: 'payee name 2', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 2', financial_year: '20/21', total_amount: 200 },
   { payee_name: 'payee name 2', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 1', financial_year: '20/21', total_amount: 300 },
   { payee_name: 'payee name 2', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 2', financial_year: '20/21', total_amount: 400 },
   { payee_name: 'payee name 3', part_postcode: 'part postcode', town: 'town', county_council: 'county council', scheme: 'scheme 1', financial_year: '21/22', total_amount: 500 },
@@ -19,7 +19,7 @@ describe('search', () => {
 
   describe('getPaymentData', () => {
     test('should return search results as an object', async () => {
-      const data = await getPaymentData({ searchString: 'payee name', limit: 10, offset: 0, sortBy: 'score', filterBy: {} })
+      const data = await getPaymentData({ searchString: 'payee name 1', limit: 10, offset: 0, sortBy: 'score', filterBy: {} })
       expect(data).toBeInstanceOf(Object)
     })
   })
